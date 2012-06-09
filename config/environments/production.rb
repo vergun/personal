bdb461b7e06d90eb014cf33b87d32d04eea8c203
Verdi Erel Ergün <vergun@gmail.com>
2012-06-08 21:42:01 -0700

    Hah

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Blog::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.assets.initialize_on_precompile = false
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
