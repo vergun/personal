0f036f44e68838752a99d6292c8b0cab99abac55
Verdi Erel Ergün <vergun@gmail.com>
2012-06-15 16:39:34 -0700

    Style changes and updating title content to newer of created_at or updated_at.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,8 +1,9 @@
 module PostsHelper
-attr_accessor :posts_size
 
-	def last_updated
-		time_ago_in_words(Post.last.updated_at)
+	def site_last_updated?
+		@posts_updated = Post.all.sort_by{|post| post.updated_at}
+		@posts_created = Post.all.sort_by{|post| post.created_at}
+		@posts_created.last.created_at > @posts_updated.last.updated_at ? time_ago_in_words(@posts_created.last.created_at) : time_ago_in_words(@posts_updated.last.updated_at)
 	end
 
 end
